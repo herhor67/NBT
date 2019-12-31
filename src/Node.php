c78693615848846cf4fa3ece5591ff84aee2ac73
herhor67 <herhor67@interia.pl>
2019-12-31 22:33:48 +0100

    Update Node.php

--- src/Node.php
+++ src/Node.php
@@ -3,179 +3,149 @@
 namespace Nbt;
 
 /**
- * We're going to rejig the node to accept the values we need to associate
- * with a node.
- */
+* We're going to rejig the node to accept the values we need to associate with a node.
+**/
 class Node implements \Tree\Node\NodeInterface
 {
-    use \Tree\Node\NodeTrait {
-        __construct as private __traitConstruct;
-    }
+	use \Tree\Node\NodeTrait
+	{
+		__construct as private __traitConstruct;
+	}
 
-    /**
-     * Create a node, optionally passing an array of children.
-     *
-     * @param array $children
-     */
-    public function __construct($children = [])
-    {
-        $this->__traitConstruct(null, $children);
-        $this->value = [];
-    }
+	/**
+	* Create a node, optionally passing an array of children.
+	* __construct([array $children])
+	**/
+	public function __construct($children = [])
+	{
+		$this->__traitConstruct(null, $children);
+		$this->value = [];
+	}
 
-    /**
-     * Set the type of this node.
-     *
-     * @param int $type Byte representing the payload type
-     */
-    public function setType($type)
-    {
-        $this->value['type'] = $type;
+	/**
+	* Set the type of this node.
+	* setType(int $type) : this
+	**/
+	public function setType($type)
+	{
+		$this->value['type'] = $type;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	* Set the name for this node.
+	* setName(string $name) : this
+	**/
+	public function setName($name)
+	{
+		$this->value['name'] = $name;
+		return $this;
+	}
 
-    /**
-     * Set the name for this node.
-     *
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->value['name'] = $name;
+	/**
+	* Set the value for this node.
+	* tagByte(mixed $value) : this
+	**/
+	public function setValue($value)
+	{
+		$this->value['value'] = $value;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	* Set the payload type for this node.
+	* setPayloadType(int $type) : this
+	**/
+	public function setPayloadType($type)
+	{
+		$this->value['payloadType'] = $type;
+		return $this;
+	}
 
-    /**
-     * Set the value for this node.
-     *
-     * @param mixed $value
-     */
-    public function setValue($value)
-    {
-        $this->value['value'] = $value;
+	/**
+	* Get the type of this node.
+	* tagByte() : int
+	**/
+	public function getType()
+	{
+		return $this->getKey('type');
+	}
 
-        return $this;
-    }
+	/**
+	* Get the name of this node.
+	* getName() : string
+	**/
+	public function getName()
+	{
+		return $this->getKey('name');
+	}
 
-    /**
-     * Set the payload type for this node.
-     *
-     * @param int $type Byte representing the payload type
-     */
-    public function setPayloadType($type)
-    {
-        $this->value['payloadType'] = $type;
+	/**
+	* Get the value for this node.
+	* getValue() : mixed
+	**/
+	public function getValue()
+	{
+		return $this->getKey('value');
+	}
 
-        return $this;
-    }
+	/**
+	* Get the payload type associated with this node.
+	* getPayloadType() : int
+	**/
+	public function getPayloadType()
+	{
+		return $this->getKey('payloadType');
+	}
 
-    /**
-     * Get the type of this node.
-     *
-     * @return int Byte representing the payload type
-     */
-    public function getType()
-    {
-        return $this->getKey('type');
-    }
+	/**
+	* Set a key for the current node.
+	* setKey(string $key, mixed $value) : void
+	**/
+	public function setKey($key, $value)
+	{
+		$this->value[$key] = $value;
+	}
 
-    /**
-     * Get the name of this node.
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->getKey('name');
-    }
+	/**
+	* Get a key for the current node.
+	* getKey(string $key) : mixed
+	**/
+	public function getKey($key)
+	{
+		if (array_key_exists($key, $this->value))
+			return $this->value[$key];
+	}
 
-    /**
-     * Get the value for this node.
-     *
-     * @return mixed
-     */
-    public function getValue()
-    {
-        return $this->getKey('value');
-    }
+	/**
+	* Strip data out of this node to make a list payload (no name or type required).
+	* makeListPayload() : this
+	**/
+	public function makeListPayload()
+	{
+		if (array_key_exists('name', $this->value))
+			unset($this->value['name']);
+		if (array_key_exists('type', $this->value))
+			unset($this->value['type']);
+		return $this;
+	}
 
-    /**
-     * Get the payload type associated with this node.
-     *
-     * @return int Byte representing the payload type
-     */
-    public function getPayloadType()
-    {
-        return $this->getKey('payloadType');
-    }
-
-    /**
-     * Set a key for the current node.
-     *
-     * @param string $key
-     * @param mixed  $value
-     */
-    public function setKey($key, $value)
-    {
-        $this->value[$key] = $value;
-    }
-
-    /**
-     * Get a key for the current node.
-     *
-     * @param string $key
-     *
-     * @return mixed
-     */
-    public function getKey($key)
-    {
-        if (array_key_exists($key, $this->value)) {
-            return $this->value[$key];
-        }
-    }
-
-    /**
-     * Strip data out of this node to make a list payload (no name or type required).
-     */
-    public function makeListPayload()
-    {
-        if (array_key_exists('name', $this->value)) {
-            unset($this->value['name']);
-        }
-
-        if (array_key_exists('type', $this->value)) {
-            unset($this->value['type']);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Find a child tag by name.
-     *
-     * @param string $name
-     *
-     * @return bool|\Nbt\Node
-     */
-    public function findChildByName($name)
-    {
-        if ($this->getName() == $name) {
-            return $this;
-        }
-
-        // A list of Compound tags has no data associated with it...
-        // so just check for children.
-        if (!$this->isLeaf()) {
-            foreach ($this->getChildren() as $childNode) {
-                $node = $childNode->findChildByName($name);
-                if ($node) {
-                    return $node;
-                }
-            }
-        }
-
-        return false;
-    }
+	/**
+	* Find a child tag by name.
+	* tagByte(string $name) : \Nbt\Node|false
+	**/
+	public function findChildByName($name)
+	{
+		if ($this->getName() == $name)
+			return $this;
+		// A list of Compound tags has no data associated with it, so just check for children.
+		if (!$this->isLeaf())
+			foreach ($this->getChildren() as $childNode)
+			{
+				$node = $childNode->findChildByName($name);
+				if ($node)
+					return $node;
+			}
+		return false;
+	}
 }
